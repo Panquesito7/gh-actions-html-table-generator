6ce846e9e82c530726f1e1047a24e5ba7f2cd0f3
Panquesito7 <halfpacho@gmail.com>
2021-08-15 18:28:36 +0000

    Update (this one might work)

--- dist/index.js
+++ dist/index.js
@@ -5577,7 +5577,7 @@ var ReadmeBox = /*#__PURE__*/function () {
         owner: _this2.owner,
         repo: _this2.repo,
         ref: _this2.branch,
-        file: path.join(process.env.GITHUB_WORKSPACE, core.getInput('file-to-use'))
+        file: core.getInput('file-to-use')
       })).then(function (_ref2) {
         var data = _ref2.data;
         // The API returns the blob as base64 encoded, we need to decode it
@@ -5602,7 +5602,7 @@ var ReadmeBox = /*#__PURE__*/function () {
         owner: _this4.owner,
         repo: _this4.repo,
         content: Buffer.from(opts.content).toString('base64'),
-        path: opts.path || path.join(process.env.GITHUB_WORKSPACE, core.getInput('file-to-use')),
+        path: opts.path || core.getInput('file-to-use'),
         message: opts.message || 'Updating the `profiles.md` file',
         sha: opts.sha,
         branch: opts.branch || 'master'
