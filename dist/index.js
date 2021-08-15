ba7f5b64c5b112e406264cb3812e82f491743464
Panquesito7 <halfpacho@gmail.com>
2021-08-15 18:12:56 +0000

    update (might not work)

--- dist/index.js
+++ dist/index.js
@@ -2993,7 +2993,7 @@ const Endpoints = {
     getPullRequestReviewProtection: ["GET /repos/{owner}/{repo}/branches/{branch}/protection/required_pull_request_reviews"],
     getPunchCardStats: ["GET /repos/{owner}/{repo}/stats/punch_card"],
     getReadme: ["GET /repos/{owner}/{repo}/readme"],
-    getFile: ["GET /repos/{owner}/{repo}/{file}"],
+    getFile: ["GET /repos/{owner}/{repo}/{blob}/{branch}/{file}"],
     getRelease: ["GET /repos/{owner}/{repo}/releases/{release_id}"],
     getReleaseAsset: ["GET /repos/{owner}/{repo}/releases/assets/{asset_id}"],
     getReleaseByTag: ["GET /repos/{owner}/{repo}/releases/tags/{tag}"],
@@ -5573,7 +5573,7 @@ var ReadmeBox = /*#__PURE__*/function () {
     try {
       var _this2 = this;
 
-      return Promise.resolve(_this2.request('GET /repos/:owner/:repo/profiles.md', {
+      return Promise.resolve(_this2.request('GET /repos/:owner/:repo/:blob/:branch/profiles.md', {
         owner: _this2.owner,
         repo: _this2.repo,
         ref: _this2.branch
